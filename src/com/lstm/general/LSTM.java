f77cda4a3090fa58656c8702bae7c6dc9919257c
Gilbert Maystre <gilbert@maystre.ch>
2015-11-16 23:22:11 -0500

    Setup of the repo

---
+++ src/com/lstm/general/LSTM.java
@@ -0,0 +1,14 @@
+/*
+ *	Author:      Gilbert Maystre
+ *	Date:        Nov 16, 2015
+ */
+
+package com.lstm.general;
+
+public class LSTM {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+    
+}
