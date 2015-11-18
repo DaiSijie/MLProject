20aab31ffd01872b37df3d86910c00cd82edac33
Gilbert Maystre <gilbert@maystre.ch>
2015-11-17 20:51:38 -0500

    Added Classifier template

---
+++ src/com/lstm/general/LSTMClassifier.java
@@ -0,0 +1,23 @@
+/*
+ *	Author:      Gilbert Maystre
+ *	Date:        Nov 17, 2015
+ */
+
+package com.lstm.general;
+
+import com.lstm.generator.Generator;
+
+public class LSTMClassifier {
+
+    public void train(){
+        //to implement
+    }
+    
+    public boolean classify(Generator generator){
+        //to implement
+        
+        return false;
+    }
+    
+    
+}
